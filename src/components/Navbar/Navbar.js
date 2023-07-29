4e1b4af5082cb39e9c00f81c691893d04fe2bd6d
roliveira527 <r.deoliveira527@gmail.com>
2023-07-29 13:03:17 +0100

    fixed deprecated React 17 render function

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,9 @@ import '../Navbar/Navbar.css'
 const Navbar = () => {
     return (
         <div className="navbar">
-            <a className="navbar-title">League Account Manager</a>
+            <div className="navbar-item"><img className="navbar-logo" src="../../../public/lol_logo.png"/></div>
+            <div className="navbar-item"><a className="navbar-title">League Account Manager</a></div>
+            <div className="navbar-item"></div>
         </div>
     )
 }
