3fa1ae4c32213097ca5f31854f9e6fe0893c1cee
Ricardo Oliveira <r.deoliveira527@gmail.com>
2023-07-29 10:58:32 +0100

    created files for basic components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Navbar from './components/Navbar/Navbar';
+
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          League App
         </p>
         <a
           className="App-link"
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../Navbar/Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <a className="navbar-title">League Account Manager</a>
+        </div>
+    )
+}
+
+export default Navbar
